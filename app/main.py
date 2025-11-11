5f551c36623c2b0338e708488f5a8a9329180a46
Franklin-Alves <antoniofranklin576@gmail.com>
2025-11-11 18:12:23 -0300

    Testando o desempenho do modelo com datasets distintos

--- app/main.py
+++ app/main.py
@@ -1,8 +1,18 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-from fastapi.responses import FileResponse
+from fastapi.responses import FileResponse, JSONResponse
+from fastapi.encoders import jsonable_encoder
 from pydantic import BaseModel
 from transformers import pipeline
+import kagglehub
+import os
+import pandas as pd
+import re
+from sklearn.metrics import accuracy_score
+
+
+# Baixando Dataset 
+path = kagglehub.dataset_download("augustop/portuguese-tweets-for-sentiment-analysis")
 
 app = FastAPI(title="API de Análise de Sentimentos")
 
@@ -14,21 +24,62 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-# Carregando o modelo multilíngue para análise de sentimentos
-modelo = pipeline(
-    "text-classification", model="tabularisai/multilingual-sentiment-analysis"
-)
+# Carregando o Modelo
+modelo = pipeline("sentiment-analysis", model="pysentimiento/bertweet-pt-sentiment")
 
-# Carregando o modelo Dataset Multilíngue (estrelas 1–5)
-# modelo = pipeline("sentiment-analysis", model="nlptown/bert-base-multilingual-uncased-sentiment")
+# Funcões e Rotas
+# Função para limpar os textos
+def clean_text(text):
+    text = re.sub(r"http\S+", "", text)  # Remove URLs
+    text = re.sub(r"@\w+", "", text)    # Remove menções
+    text = re.sub(r"#\w+", "", text)    # Remove hashtags
+    text = re.sub(r"[^a-zA-Záéíóúçãõ ]", " ", text)  # Remove caracteres especiais
+    text = text.lower()  # Converte para minúsculas
+    return text
+
+# Função de avalição dos datasets
+def avaliacaoDF():
+    # Ler o Arquivo dataset Tweet
+    # arquivo = pd.read_csv(os.path.join(path, "TweetsWithTheme.csv"))
+    # df = pd.DataFrame(arquivo)
+    # abrev = {"Positivo": "POS", "Negativo": "NEG"}
+    # df['sentimento'] = df["sentiment"].replace(abrev)
+
+    # Ler o Arquivo dataset Curso
+    df = pd.read_csv("app/dataset_comentarios_curso.csv")
+
+    # Ler o Arquivo dataset Prdouto
+    # df = pd.read_csv("app/dataset_comentarios_produto.csv")
+
+    #Limpa os textos dataset do Curso e Produto
+    df["texto_limpo"] = df["comentario"].apply(clean_text)
+
+    #Limpa os textos do dataset do Tweet
+    # df["texto_limpo"] = df["tweet_text"].apply(clean_text)
+
+    #Separando os textos e os labels
+    x_teste = df["texto_limpo"].head(1000)
+    x_teste_lista = x_teste.dropna().astype(str).tolist()
+    y_teste = df["sentimento"].head(1000)
 
 
-# Carregando o Modelo NILC (USP)
-# modelo = pipeline("sentiment-analysis", model="pysentimiento/bertweet-pt-sentiment")
+    # Fazendo a previsão do modelo
+    y_pred = list(modelo(x_teste_lista, batch_size=30))
+    predic = [r["label"] for r in y_pred]
 
+    # Criando DataFrame comparativo
+    comparacao = pd.DataFrame({
+        "texto": x_teste,
+        "sentimento_real": y_teste,
+        "sentimento_previsto": predic
+    })
 
-# Carregando o modelo XLM-R (Twitter, multilíngue)
-# modelo = pipeline("sentiment-analysis", model="pysentimiento/bertweet-pt-sentiment")
+    # Filtrando erros
+    erros = comparacao[comparacao["sentimento_real"] != comparacao["sentimento_previsto"]]
+
+    #Avaliando a acurácia
+    acc = accuracy_score(y_teste, predic)
+    return  acc, erros
 
 
 class TextoEntrada(BaseModel):
@@ -39,12 +90,13 @@ class TextoEntrada(BaseModel):
 def frontend():
     return FileResponse("app/frontend.html")
 
-
-@app.post("/analisar")
-async def analisar_sentimento(dados: TextoEntrada):
-    resultado = modelo(dados.texto)[0]
-    return {
-        "texto": dados.texto,
-        "sentimento": resultado["label"],
-        "confianca": round(resultado["score"], 4),
-    }
+@app.get("/dados")
+async def dados():
+    try:
+        acuracia, errosT = avaliacaoDF()
+        return JSONResponse(content={
+            "avaliacao": acuracia,
+            "erros": jsonable_encoder(errosT.head(10).to_dict(orient="records"))
+        })
+    except Exception as e:
+        return JSONResponse(content={"erro": str(e)}, status_code=500)
