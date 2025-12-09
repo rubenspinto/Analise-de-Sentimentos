a6568caa5b17b41dc5237829572c7e3fe7a8c686
Franklin-Alves <antoniofranklin576@gmail.com>
2025-12-09 09:32:42 -0300

    fix: Corrigindo erro de leitura de arquivos .ods

--- app/main.py
+++ app/main.py
@@ -1,14 +1,11 @@
-from fastapi import FastAPI, UploadFile, Request
+from fastapi import FastAPI, UploadFile
 from fastapi.middleware.cors import CORSMiddleware
-from fastapi.responses import FileResponse, JSONResponse, HTMLResponse
-from fastapi.encoders import jsonable_encoder
+from fastapi.responses import FileResponse, HTMLResponse
 from pydantic import BaseModel
 from transformers import pipeline
 from starlette.background import BackgroundTask
-import kagglehub
 import os
 import pandas as pd
-import re
 
 
 
@@ -26,11 +23,16 @@ app.add_middleware(
 modelo = pipeline("sentiment-analysis", model="pysentimiento/bertweet-pt-sentiment")
 
 # Funcões e Rotas
-def delete_file(path):
+
+
+
+def delete_file(path): # Função para deletar arquivo após download
+
     if os.path.exists(path):
         os.remove(path)
 
 def avaliandoArquivo(df, textos):
+
     # Separando os textos
     x_campo = textos
     x_campo_lista = x_campo.astype(str).tolist()
@@ -40,7 +42,7 @@ def avaliandoArquivo(df, textos):
     y_pred_campo = list(modelo(x_campo_lista, batch_size=30))
     predic_campo = [r["label"] for r in y_pred_campo]
 
-
+    # Adicionando as previsões ao DataFrame
     df["Sentimento dos Alunos"] = predic_campo
 
 
@@ -56,9 +58,12 @@ def frontend():
     
 @app.post("/analisar_arquivo")
 async def upload_planilha(file: UploadFile):
-    if not file.filename.endswith(".xlsx"):
-        return JSONResponse(content={"erro": "Formato inválido. Use xlsx."}, status_code=400)
 
+    # Verifica o formato do arquivo
+    if not file.filename.endswith(".xlsx") and not file.filename.endswith(".ods"):
+        return HTMLResponse("<h3>Formato de arquivo não suportado. Por favor, envie um arquivo .xlsx ou .ods</h3>")
+    
+    # Cria pasta temporária para salvar o arquivo
     os.makedirs("temp", exist_ok=True)
     temp_path = os.path.join("temp", file.filename)
     
@@ -67,28 +72,48 @@ async def upload_planilha(file: UploadFile):
         with open(temp_path, "wb") as f:
             f.write(await file.read())
 
+        # Lê a planilha
+        dfPlanilha = pd.read_excel(temp_path, skiprows=1, sheet_name=1)
+
+        # Preenche valores nulos na coluna de análise de texto
+        textosAnalise = dfPlanilha["Qual sua mensagem, dica, sugestão ou crítica para o programa?"].fillna("Normal")
+
+        # Faz a análise de sentimentos
+        previsoes = avaliandoArquivo(dfPlanilha, textosAnalise)
+
+        nome, extensao = os.path.splitext(file.filename) # Pega a extensão do arquivo
+        output_path = nome + "_com_sentimentos" + extensao   # Nome do arquivo de saída
+        previsoes.to_excel(output_path, index=False) # Salva o arquivo de saída
+
+        task = BackgroundTask(delete_file, output_path) # Deleta o arquivo após o download
+
         if file.filename.endswith(".xlsx"):
-            dfPlanilha = pd.read_excel(temp_path, skiprows=1, sheet_name=1)
-            textosAnalise = dfPlanilha["Qual sua mensagem, dica, sugestão ou crítica para o programa?"].fillna("Normal")
-
-            previsoes = avaliandoArquivo(dfPlanilha, textosAnalise)
-            output_path = "planilha_analisada.xlsx"
-            previsoes.to_excel(output_path, index=False)
-
-            task = BackgroundTask(delete_file, output_path)
-
-
+            
+            # Retorna o arquivo para download em XLSX
             response = FileResponse(
                 output_path, 
-                filename="planilha_analisada.xlsx", 
+                filename=output_path, 
                 media_type='application/vnd.openxmlformats-officedocument.spreadsheetml.sheet',
                 background=task
             )
             response.set_cookie(key="downloadFinalizado", value="1")
+
+        if file.filename.endswith(".ods"):
+
+            # Retorna o arquivo para download em ODS
+            response = FileResponse(
+                output_path, 
+                filename=output_path, 
+                media_type='application/vnd.oasis.opendocument.spreadsheet',
+                background=task
+            )
+            response.set_cookie(key="downloadFinalizado", value="1")
+
         return response
 
     except Exception as e:
-        return JSONResponse(content={"erro": f"Erro ao salvar o arquivo: {str(e)}"}, status_code=500)
+        # Em caso de erro, retorna uma mensagem de erro
+        return HTMLResponse(f"<h3>Erro ao processar o arquivo: {str(e)}</h3>")
     
     finally:
         # Remove o arquivo temporário
